2e35096c5dd4a30fa202638318f3c35637c08e05
Sebastian Ospina <kraige@gmail.com>
2015-01-08 22:35:05 -0600

    Fixing tabs, removing unnecesary printfs, double substractions

--- ex1-14.c
+++ ex1-14.c
@@ -2,24 +2,24 @@
 // character frequency "bars"
 int main(int argc, char *argv[]){
 	int c;
-	int cfreq['~'-'!'];
+	unsigned short int range = '~'-'!';
+	int cfreq[range];
 	int i,j;
-    unsigned short int range;
-    range = '~'-'!';
 	for(i = 0; i < range; ++i)
 		cfreq[i]=0;
 	while((c = getchar()) != EOF){
-		if(c >= '!' && c <= '~'){
-			++cfreq[c-'!'];
+		c-='!';
+		if(c <= range){
+			++cfreq[c];
 		}
 	}
 	printf("character frequency:");
 	for(i = 0; i < range; ++i)
 		if(cfreq[i]){
-			printf("%c:",i+'!');
+			putchar(i+'!');
 			for(j=0;j<cfreq[i];j++)
-				printf(".");
-			printf("\n");
+				putchar('.');
+			putchar('\n');
 		}
 	return 0;
 }
