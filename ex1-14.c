c57c5cfec5d52ea25602c6d4a4ed3220dd90f139
Sebastian Ospina <kraige@gmail.com>
2015-01-08 21:41:12 -0600

    Adding initial exercise 1-14

---
+++ ex1-14.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+// character frequency "bars"
+int main(int argc, char *argv[]){
+	int c;
+	int cfreq['~'-'!'];
+	int i,j;
+	for(i = 0; i < '~'-'!'; ++i)
+		cfreq[i]=0;
+	while((c = getchar()) != EOF){
+		if(c >= '!' && c <= '~'){
+			++cfreq[c-'!'];
+		}
+	}
+	printf("character frequency:");
+	for(i = 0; i < '~'-'!'; ++i)
+		if(cfreq[i]){
+			printf("%c:",i);
+			for(j=0;j<cfreq[i];j++)
+				printf(".");
+		}
+	printf("\n");
+	return 0;
+}
