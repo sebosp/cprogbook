3264f79df167e7ae27b5296c65d37290bb08f537
Marvin Ortega <isaiasmarvin@gmail.com>
2015-01-08 21:09:55 -0600

    Mispelled word in comment. Uninitialized variable bug fixed. Removing unnecessary variables from printf() function since it is inteded as the header of the convertion table (also it breaks the code).

--- ex1-4.c
+++ ex1-4.c
@@ -7,7 +7,8 @@ int main(int argc, char *argv[]){
 	upper = 300;
 	step = 20;
 	fahr = lower;
-	printf("CELSIUS\tFAHR\n",fahr,celsius);
+    celsius=0;
+	printf("CELSIUS\tFAHR\n"); // This is the header of the convertion table
 	while(fahr <= upper){
 		celsius = (5.0/9.0) * (fahr - 32.0);
 		printf("%3.0f\t%6.1f\n",celsius,fahr);
