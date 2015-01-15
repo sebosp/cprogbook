0f8b73f2ed28f22396ea0db9253581179143008d
Sebastian Ospina <kraige@gmail.com>
2015-01-14 22:27:52 -0600

    Adding line cutter using maxlinechars constant and outputting first 100 chars

---
+++ ex1-16.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#define MAXLINECHARS 500
+
+int getlittleline(char line[]);
+
+int main(int argc, char *argv[]){
+	int len;
+	char line[MAXLINECHARS];
+	int linenum=0;
+	while((len = getlittleline(line)) > 0){
+		++linenum;
+		if(len == MAXLINECHARS - 1){
+			printf("Line %d cut at %d max characters. First 100 chars: %.100s\n",linenum,MAXLINECHARS,line);
+		}
+	}
+	return 0;
+}
+
+int getlittleline(char line[]){
+	int cinput,idx;
+	for(idx=0;idx < MAXLINECHARS - 1 && (cinput = getchar()) != EOF && cinput != '\n'; idx++)
+		line[idx]=cinput;
+	if(cinput == '\n'){
+		line[idx++]='\n';
+	}
+	line[idx] = '\0';
+	return idx;
+}
