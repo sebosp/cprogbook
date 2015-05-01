3ab4c674d91f1058f05e8f831a4b1abe1a331ea9
Sebastian Ospina <kraige@gmail.com>
2015-05-01 13:34:13 -0600

    Removing almost-serializing initial attempt inside the loop

--- openmp/exc1g.c
+++ openmp/exc1g.c
@@ -8,27 +8,22 @@ int
 main(int argc, char *argv[])
 {
 	double pi = 0.0;
-	double sum = 0.0;
-	int i,nthreads = 0;
 	step = 1.0/(double) num_steps;
 	double ini_time = omp_get_wtime();
 	# pragma omp parallel
 	{
 		double x;
 		int i,id,nthrds;
+		double sum = 0.0;
 		id = omp_get_thread_num();
 		nthrds = omp_get_num_threads();
-		if (id == 0)
-			nthreads = nthrds; 
 		for(i = id; i < num_steps; i+=nthrds){
-			# pragma omp critical
-			{
 			x = (i + 0.5) * step;
 			sum += 4.0/(1.0 + x * x);
-			}
 		}
+		# pragma omp critical
+		pi += step * sum;
 	}
-	pi = step * sum;
 	printf("Duration: [%f] result: [%f]\n",omp_get_wtime() - ini_time,pi);
 	return 0;
 }
